0140fcbecbf32fcf61403033e0812b6df93a9c3e
BeesQ <jakublotysz@gmail.com>
2022-06-10 00:40:30 +0200

    Hello World!

--- Assets/HelloWorld.cs
+++ Assets/HelloWorld.cs
@@ -7,7 +7,7 @@ public class HelloWorld : MonoBehaviour
     // Start is called before the first frame update
     void Start()
     {
-        
+        Debug.Log("Hello World!");
     }
 
     // Update is called once per frame
