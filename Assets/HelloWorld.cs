a2f264c4d3d95b44643385c4ac4e96e79a87aa63
BeesQ <jakublotysz@gmail.com>
2022-06-10 00:46:55 +0200

    Test

--- Assets/HelloWorld.cs
+++ Assets/HelloWorld.cs
@@ -8,6 +8,8 @@ public class HelloWorld : MonoBehaviour
     void Start()
     {
         Debug.Log("Hello World!");
+        Debug.Log("Hello World!");
+        Debug.Log("Hello World!");
     }
 
     // Update is called once per frame
