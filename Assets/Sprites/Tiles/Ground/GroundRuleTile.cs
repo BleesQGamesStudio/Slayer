03584c2cc842dc2aafce1d1ea565c43aab1b73a3
BeesQ <jakublotysz@gmail.com>
2022-06-13 01:54:12 +0200

    IT WORKS!!!... but need some fixes xd

--- Assets/Sprites/Tiles/Ground/GroundRuleTile.cs
+++ Assets/Sprites/Tiles/Ground/GroundRuleTile.cs
@@ -6,11 +6,18 @@ using UnityEngine.Tilemaps;
 
 [CreateAssetMenu(menuName = "My Tools/Custom Tiles/Ground Rule Tile")]
 public class GroundRuleTile : RuleTile<GroundRuleTile.Neighbor> {
+    [Header("Advanced Tile")]
+    [Tooltip("If enabled, the tile will connect to these tiles too when the mode is set to \"This\"")]
+    public bool alwaysConnect;
+
     [Tooltip("Tiles to connect to")]
     public TileBase[] grass;
+
     [Space]
+
     [Tooltip("Tiles to connect to")]
     public TileBase[] dirt;
+
     public bool checkSelf = true;
     
     public class Neighbor : RuleTile.TilingRule.Neighbor
@@ -21,24 +28,45 @@ public class GroundRuleTile : RuleTile<GroundRuleTile.Neighbor> {
  
     public override bool RuleMatch(int neighbor, TileBase tile)
     {
+        // Debug.Log(neighbor);
+        // Debug.Log(tile);
         switch (neighbor)
         {
+            case Neighbor.This: return CheckThis(tile);
+            case Neighbor.NotThis: return CheckNotThis(tile);
             case Neighbor.Grass: return CheckGrass(tile);
             case Neighbor.Dirt: return CheckDirt(tile);
         }
         return base.RuleMatch(neighbor, tile);
     }
 
+    bool CheckThis(TileBase tile)
+    {
+        if (!alwaysConnect) return tile == this;
+        else return  (dirt.Contains(tile) || grass.Contains(tile)) || tile == this;
+        //.Contains requires "using System.Linq;"
+    }
+
+    bool CheckNotThis(TileBase tile)
+    {
+        if (!alwaysConnect) return tile != this;
+        else return !dirt.Contains(tile) && !grass.Contains(tile) && tile != this;
+        //.contains requires "using system.linq;"
+    }
+
     private bool CheckGrass(TileBase tile)
     {
-        if (checkSelf) return tile != null;
-        else return tile != null && tile != this && tile == grass.Contains(tile);
+        return grass.Contains(tile);
+        // Debug.Log(tile == grass.Contains(tile));
+        // if (checkSelf) return tile != null;
+        // else return tile != null && tile != this && tile == grass.Contains(tile);
     }
 
     private bool CheckDirt(TileBase tile)
     {
-        if (checkSelf) return tile != null;
-        else return tile != null && tile != this && tile == dirt.Contains(tile);
+        return dirt.Contains(tile);
+        // if (checkSelf) return tile != null;
+        // else return tile != null && tile != this && tile == dirt.Contains(tile);
     }
 }
 
