3a221a8e28e356b803794940fbd0e134fc1ffdd7
BeesQ <jakublotysz@gmail.com>
2022-06-15 00:05:02 +0200

    Collision Added

--- Assets/Scripts/Player.cs
+++ Assets/Scripts/Player.cs
@@ -2,14 +2,19 @@ using System.Collections;
 using System.Collections.Generic;
 using UnityEngine;
 
-public class Player : MonoBehaviour
+public class Player : MonoBehaviour, IDamageable<float>
 {
     [SerializeField] PlayerMovement myMovement = null;
     [SerializeField] PlayerRotation myRotation = null;
 
-    void FixedUpdate()
+    private void FixedUpdate()
     {
         myMovement.HandleMovement();
         myRotation.HandleRotating();
+    }
+
+    public void TakeDamage(float amount)
+    {
+        Debug.Log($"Uderzono Gracza za ${amount}");
     }
 }
